45f0592bb83a3d7a32f7cac3c309f96e28e139d3
unknown <miguel.barrera-d@labinfo.is.escuelaing.edu.co>
2023-03-16 18:08:04 -0500

    first commit

---
+++ src/test/java/com/miguel/barrera/springbootjpamediumdemo/SpringBootJpaMediumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.barrera.springbootjpamediumdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaMediumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
