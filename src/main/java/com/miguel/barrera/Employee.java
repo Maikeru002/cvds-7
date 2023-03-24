d79a218fb4c654a890f04c4478ed79df33296a40
Miguel Barrera <diazbarrera90355@gmial.com>
2023-03-23 22:21:26 -0500

    Entrega Final

---
+++ src/main/java/com/miguel/barrera/Employee.java
@@ -0,0 +1,98 @@
+package com.miguel.barrera;
+
+import jakarta.persistence.*;
+import java.util.*;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long employeeId;
+	private String firstName;
+	private String lastName;
+	private String role;
+	private Double salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(String newFirstName, String newLastName, String newRole, double newSalary) {
+		firstName = newFirstName;
+		lastName = newLastName;
+		role = newRole;
+		salary = newSalary;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Employee employee = (Employee) o;
+		return Objects.equals(employeeId, employee.employeeId) && Objects.equals(firstName, employee.firstName)
+				&& Objects.equals(lastName, employee.lastName) && Objects.equals(role, employee.role)
+				&& Objects.equals(salary, employee.salary);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(employeeId, firstName, lastName, role, salary);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" +
+				"employeeId=" + employeeId +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", role='" + role + '\'' +
+				", salary=" + salary +
+				'}';
+	}
+
+	// getters
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	// setters
+	public void setEmployeeId(Long newEmployeeID) {
+		employeeId = newEmployeeID;
+	}
+
+	public void setFirstName(String newFirstName) {
+		firstName = newFirstName;
+	}
+
+	public void setLastName(String newLastName) {
+		lastName = newLastName;
+	}
+
+	public void setRole(String newRole) {
+		role = newRole;
+	}
+
+	public void setSalary(Double newSalary) {
+		salary = newSalary;
+	}
+
+}
--- src/main/java/com/miguel/barrera/EmployeeService.java
+++ src/main/java/com/miguel/barrera/EmployeeService.java
@@ -1,50 +1,41 @@
-package com.miguel.barrera.service;
+package com.miguel.barrera;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.miguel.barrera.model.Employee;
-import com.miguel.barrera.repository.EmployeeRepository;
-
-
-import java.util.List;
-
-
 @Service
 public class EmployeeService {
-	
+
 	private final EmployeeRepository employeeRepository;
-	
+
 	@Autowired
 	public EmployeeService(EmployeeRepository employeeRepository) {
 		this.employeeRepository = employeeRepository;
 	}
-	
+
 	public Employee addEmployee(Employee employee) {
 		return employeeRepository.save(employee);
 	}
-	
+
 	public Employee getEmployee(Long employeeId) {
 		return employeeRepository.findById(employeeId).get();
 	}
-	
+
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
+
 	public Employee updateEmployee(Employee employee) {
-		if(employeeRepository.existsById(employee.getEmployeeId())) {
+		if (employeeRepository.existsById(employee.getEmployeeId())) {
 			return employeeRepository.save(employee);
 		}
-		
+
 		return null;
 	}
-	
+
 	public void deleteEmployee(Long employeeId) {
 		employeeRepository.deleteById(employeeId);
-	}	
-
-
+	}
 }
